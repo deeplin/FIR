03e10148e996b77c41915f5465c1510a0e65c3a9
deeplin <10525677@qq.com>
2017-06-15 11:25:51 +0800

    first commit

---
+++ FinanceInfoRetriever/FinanceInfoRetriever/Views/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinanceInfoRetriever.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Start all services
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+        }
+
+        private Button mOldButton = new Button();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+            button.Foreground = Brushes.Red;
+            mOldButton.Foreground = Brushes.Blue;
+            mOldButton = button;
+            Uri uri = new Uri(button.Tag.ToString(), UriKind.Relative);
+            detailFrame.Source = new Uri(button.Tag.ToString(), UriKind.Relative);
+        }
+    }
+}
---
+++ FinanceInfoRetriever/FinanceInfoRetriever/Views/SetupPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinanceInfoRetriever.Views
+{
+    /// <summary>
+    /// Interaction logic for SetupPage.xaml
+    /// </summary>
+    public partial class SetupPage : Page
+    {
+        private readonly NLog.Logger mLogger = NLog.LogManager.GetCurrentClassLogger();
+
+        public SetupPage()
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            string name = btn.Name;
+
+        }
+
+        private void StartServer()
+        {
+
+        }
+
+        private void StopServer()
+        {
+
+        }
+
+        private int count = 0;
+        public void Display(string message)
+        {
+
+        }
+    }
+}
