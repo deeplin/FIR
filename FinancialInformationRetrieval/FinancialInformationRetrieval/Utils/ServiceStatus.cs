248bb440d36e0a3e0506080a1e7cdbf765ebd8ce
deeplin <10525677@qq.com>
2017-06-27 11:16:48 +0800

    re constructureed

---
+++ FinancialInformationRetrieval/FinancialInformationRetrieval/Utils/ServiceStatus.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialInformationRetrieval.Utils
+{
+    public class ServiceStatus : INotifyPropertyChanged
+    {
+        public ServiceStatus()
+        {
+            enabled = false;
+        }
+
+        private bool enabled;
+        public bool Enabled
+        {
+            get
+            {
+                return enabled;
+            }
+            set
+            {
+                SetProperty(ref enabled, value);
+            }
+        }
+        public bool Disabled
+        {
+            get
+            {
+                return !enabled;
+            }
+            set
+            {
+                SetProperty(ref enabled, !value);
+            }
+        }
+
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Enabled"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Disabled"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ FinancialInformationRetrieval/FinancialInformationRetrieval/Views/SetupPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using FinancialInformationRetrieval.Models;
+using FinancialInformationRetrieval.Utils;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FinancialInformationRetrieval.Views
+{
+    /// <summary>
+    /// Interaction logic for SetupPage.xaml
+    /// </summary>
+    public partial class SetupPage : Page
+    {
+        private readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public SetupPage()
+        {
+        }
+
+        private void Page_Initialized(object sender, EventArgs e)
+        {
+            IUnityContainer container = UnityConfig.GetConfiguredContainer();
+
+            SystemMetaData searchMetaData = container.Resolve<SystemMetaData>();
+            DataGridWebSite.ItemsSource = searchMetaData.WebSiteList;
+        }
+
+        private void DataGridWebSite_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DataGrid datagrid = sender as DataGrid;
+            object obj = datagrid.CurrentItem;
+
+            if (obj != null)
+            {
+                WebSite webSite = obj as WebSite;
+                Process.Start(new ProcessStartInfo(webSite.SiteAddress));
+            }
+        }
+
+        private void DataGridWebSite_CurrentCellChanged(object sender, EventArgs e)
+        {
+            labelStatus.Content = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+            string status = "";
+            if (button == buttonSave)
+            {
+                Save();
+                status = "已经保存";
+            }
+            labelStatus.Content = status;
+        }
+
+        private void Save()
+        {
+            IUnityContainer container = UnityConfig.GetConfiguredContainer();
+            SystemMetaData systemSetting = container.Resolve<SystemMetaData>();
+            XmlUtil.SaveToXml<List<WebSite>>(Constant.WEB_SITE_FILE, systemSetting.WebSiteList);
+        }
+    }
+}
